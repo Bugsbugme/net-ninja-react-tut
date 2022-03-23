e45599ac9ef1e8cd941d791942894f4771d26b13
Bugsbugme <8654808+Bugsbugme@users.noreply.github.com>
2022-03-23 21:56:08 +1300

    vid 11 progress

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,18 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
+  ]);
 
-  const handleClick = () => {
-    console.log('hello, ninjas')
-  }
-  
-
-  return ( 
+  return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click Me</button>
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
-   );
-}
- 
+  );
+};
+
 export default Home;
